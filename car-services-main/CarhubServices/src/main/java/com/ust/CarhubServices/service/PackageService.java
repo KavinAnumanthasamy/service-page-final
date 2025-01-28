f3511c7616caf509bb6e27d6fd64b2aa27929244
Kavin <132369155+KavinAnumanthasamy@users.noreply.github.com>
2025-01-28 17:50:07 +0530

    car service backend

---
+++ car-services-main/CarhubServices/src/main/java/com/ust/CarhubServices/service/PackageService.java
@@ -0,0 +1,31 @@
+package com.ust.CarhubServices.service;
+
+
+import com.ust.CarhubServices.entity.Package;
+import com.ust.CarhubServices.entity.CarService;
+import com.ust.CarhubServices.repository.PackageRepository;
+import com.ust.CarhubServices.repository.CarServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PackageService {
+    @Autowired
+    private PackageRepository packageRepository;
+
+    @Autowired
+    private CarServiceRepository carServiceRepository;
+
+    public List<Package> getAllPackages() {
+        return packageRepository.findAll();
+    }
+
+    public Package createPackage(Package carPackage, List<Long> carServiceIds) {
+        List<CarService> carServices = carServiceRepository.findAllById(carServiceIds);
+        carPackage.setCarServices(carServices);
+        carPackage.setTotalPrice(carServices.stream().mapToDouble(CarService::getPrice).sum());
+        return packageRepository.save(carPackage);
+    }
+}
